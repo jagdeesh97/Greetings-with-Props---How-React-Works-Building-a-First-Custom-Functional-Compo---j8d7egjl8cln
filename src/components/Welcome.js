c287f8bebce0a2e4c3cccff708456a0497653d60
recscse <recs.cse17903@gmail.com>
2023-02-25 14:04:27 +0530

    Greeting-with-props

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,10 @@ import React from "react";
 const Welcome= (name)=>{
     return(
         <div>
-          <h1>{"Hey !"}</h1>
-          <h2>{"Welcome to Newton School."}</h2>
+            <h1>{"Hey !"}
+               <h2>{"Welcome to Newton School."}</h2>
+            </h1>
+          
         </div>
     );
 };
